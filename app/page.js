51a26a274fa8fba9a9b6e520c1875d2fd331a0e2
vel-gaurea <velsongaurea@gmail.com>
2025-02-26 12:52:16 +0530

    Setup Nextjs and Shadcn UI

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>Hello World
+    <br/>
+    <Button>Button</Button>
+   </div>
+  );
+}
